4a5af17fcf177d75f14589c021dcdb93765e3e82
Andre <andreong28@gmail.com>
2021-09-26 20:25:59 +0800

    Implemented property for ID and DESC

---
+++ src/main/java/com/euph28/tson/context/keyword/PropertyDescription.java
@@ -0,0 +1,44 @@
+package com.euph28.tson.context.keyword;
+
+import com.euph28.tson.context.TSONContext;
+import com.euph28.tson.context.VariableType;
+import com.euph28.tson.interpreter.keyword.Keyword;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility keyword: Description Property
+ * <p>
+ * Set the Test description property
+ */
+public class PropertyDescription extends Keyword {
+    /* ----- VARIABLES ------------------------------ */
+    Logger logger = LoggerFactory.getLogger(PropertyDescription.class);
+
+    /* ----- OVERRIDE: KEYWORD ------------------------------ */
+    @Override
+    public String getCode() {
+        return "DESC";
+    }
+
+    @Override
+    public String getDescriptionShort() {
+        return "Set the description of this test";
+    }
+
+    @Override
+    public String getDescriptionLong() {
+        return "Set the description of this test. Subsequent calls will override the existing description";
+    }
+
+    @Override
+    public boolean isAction() {
+        return false;
+    }
+
+    @Override
+    public boolean handle(TSONContext tsonContext, String value) {
+        tsonContext.addVariable(VariableType.PROPERTY, "desc", value);
+        return true;
+    }
+}
---
+++ src/main/java/com/euph28/tson/context/keyword/PropertyId.java
@@ -0,0 +1,44 @@
+package com.euph28.tson.context.keyword;
+
+import com.euph28.tson.context.TSONContext;
+import com.euph28.tson.context.VariableType;
+import com.euph28.tson.interpreter.keyword.Keyword;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility keyword: ID Property
+ * <p>
+ * Set the Test ID property
+ */
+public class PropertyId extends Keyword {
+    /* ----- VARIABLES ------------------------------ */
+    Logger logger = LoggerFactory.getLogger(PropertyId.class);
+
+    /* ----- OVERRIDE: KEYWORD ------------------------------ */
+    @Override
+    public String getCode() {
+        return "ID";
+    }
+
+    @Override
+    public String getDescriptionShort() {
+        return "Set the ID of this test";
+    }
+
+    @Override
+    public String getDescriptionLong() {
+        return "Set the ID of this test. Subsequent calls will override the existing ID";
+    }
+
+    @Override
+    public boolean isAction() {
+        return false;
+    }
+
+    @Override
+    public boolean handle(TSONContext tsonContext, String value) {
+        tsonContext.addVariable(VariableType.PROPERTY, "id", value);
+        return true;
+    }
+}
