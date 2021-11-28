671d2fb8b5ef720f05386ae105e5c5385506cc22
Andre <andreong28@gmail.com>
2021-11-29 05:36:14 +0800

    feature: Implemented properties for Statements - Entries in TSON file can now add properties by entering space delimited key-value properties enclosed in square brackets after a Keyword - eg: ID [title="This is a title" desc="Here's another one"] Actual ID - If there is no key, title will be used

--- src/main/java/com/euph28/tson/interpreter/Statement.java
+++ src/main/java/com/euph28/tson/interpreter/Statement.java
@@ -4,6 +4,8 @@ import com.euph28.tson.core.keyword.Keyword;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
 /**
  * Represents a line within a TSON file. A statement is formed starting with a {@link Keyword} followed by values
  */
@@ -17,6 +19,11 @@ public class Statement {
     Keyword keyword;
 
     /**
+     * Mapping of additional properties in value
+     */
+    Map<String, String> properties;
+
+    /**
      * Values accompanying the {@link Keyword}
      */
     String value;
@@ -27,11 +34,13 @@ public class Statement {
      * Create a statement. Statements are lines within a TSON file that starts with a {@link Keyword} followed
      * by {@code value} that should be passed to the {@link Keyword} to be handled
      *
-     * @param keyword The {@link Keyword} of this statement
-     * @param value   Values to be passed to the {@link Keyword}
+     * @param keyword    The {@link Keyword} of this statement
+     * @param properties Map of properties for this statement
+     * @param value      Values to be passed to the {@link Keyword}
      */
-    public Statement(Keyword keyword, String value) {
+    public Statement(Keyword keyword, Map<String, String> properties, String value) {
         this.keyword = keyword;
+        this.properties = properties;
         this.value = value;
     }
 
@@ -44,6 +53,17 @@ public class Statement {
      */
     public Keyword getKeyword() {
         return keyword;
+    }
+
+    /**
+     * Retrieve property value of the provided key
+     *
+     * @param key          Key for the value
+     * @param defaultValue Default value to retrieve if there is no entry
+     * @return Value of the provided key
+     */
+    public String getProperty(String key, String defaultValue) {
+        return properties.getOrDefault(key, defaultValue);
     }
 
     /**
